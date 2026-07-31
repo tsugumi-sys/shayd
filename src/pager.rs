c22acf495404efdad6f871496941c3457a253044
Akira Noda <61897166+tsugumi-sys@users.noreply.github.com>
2026-07-31 17:37:25 +0900

    abstract page source

--- src/pager.rs
+++ src/pager.rs
@@ -8,7 +8,7 @@ use crate::header::DatabaseHeader;
 #[derive(Debug)]
 pub struct Pager {
     path: PathBuf,
-    file: File,
+    source: Box<dyn PageSource>,
     header: DatabaseHeader,
     database_size_pages: u32,
 }
@@ -22,11 +22,15 @@ pub struct Page {
 impl Pager {
     pub fn open(path: impl AsRef<Path>) -> Result<Self> {
         let path = path.as_ref().to_owned();
-        let mut file = File::open(&path)?;
+        let source = FilePageSource::open(&path)?;
+        Self::from_source(path, Box::new(source))
+    }
+
+    fn from_source(path: PathBuf, mut source: Box<dyn PageSource>) -> Result<Self> {
         let mut header_bytes = [0; DatabaseHeader::SIZE];
-        file.read_exact(&mut header_bytes)?;
+        source.read_exact_at(0, &mut header_bytes)?;
         let header = DatabaseHeader::parse(&header_bytes)?;
-        let file_size = file.metadata()?.len();
+        let file_size = source.len()?;
         let page_size = u64::from(header.page_size.get());
         let file_size_pages = u32::try_from(file_size / page_size)
             .map_err(|_| Error::InvalidDatabaseHeader("database file is too large"))?;
@@ -45,7 +49,7 @@ impl Pager {
 
         Ok(Self {
             path,
-            file,
+            source,
             header,
             database_size_pages,
         })
@@ -76,12 +80,41 @@ impl Pager {
         let page_size = self.header.page_size.get() as usize;
         let offset = u64::from(page_number - 1) * page_size as u64;
         let mut bytes = vec![0; page_size];
-        self.file.seek(SeekFrom::Start(offset))?;
-        self.file.read_exact(&mut bytes)?;
+        self.source.read_exact_at(offset, &mut bytes)?;
         Ok(Page {
             number: page_number,
             bytes,
         })
+    }
+}
+
+trait PageSource: std::fmt::Debug {
+    fn len(&self) -> Result<u64>;
+    fn read_exact_at(&mut self, offset: u64, buf: &mut [u8]) -> Result<()>;
+}
+
+#[derive(Debug)]
+struct FilePageSource {
+    file: File,
+}
+
+impl FilePageSource {
+    fn open(path: &Path) -> Result<Self> {
+        Ok(Self {
+            file: File::open(path)?,
+        })
+    }
+}
+
+impl PageSource for FilePageSource {
+    fn len(&self) -> Result<u64> {
+        Ok(self.file.metadata()?.len())
+    }
+
+    fn read_exact_at(&mut self, offset: u64, buf: &mut [u8]) -> Result<()> {
+        self.file.seek(SeekFrom::Start(offset))?;
+        self.file.read_exact(buf)?;
+        Ok(())
     }
 }
 
