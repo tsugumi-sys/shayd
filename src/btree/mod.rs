235792268667babb059f61697e99b573cffae31a
Akira Noda <61897166+tsugumi-sys@users.noreply.github.com>
2026-08-27 12:01:08 +0900

    cleanup

--- src/btree/mod.rs
+++
@@ -1,8 +0,0 @@
-mod index;
-mod page;
-mod payload;
-mod table;
-
-pub use index::IndexLeafCell;
-pub use page::{BtreePage, BtreePageHeader, PageType};
-pub use table::{TableInteriorCell, TableLeafCell, TableLeafPayload};
--- tests/common/mod.rs
+++
@@ -1,8 +0,0 @@
-use std::path::PathBuf;
-
-pub fn fixture_path(name: &str) -> PathBuf {
-    PathBuf::from(env!("CARGO_MANIFEST_DIR"))
-        .join("tests")
-        .join("fixtures")
-        .join(name)
-}
